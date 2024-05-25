87ca5abaf880ac4531152be1409aa69fad97c69b
Sreedhar1729 <sreedharsreeram04@gmail.com>
2024-05-25 10:47:26 +0000

    multi del

--- app/centrallibrary/webapp/controller/Login.controller.js
+++ app/centrallibrary/webapp/controller/Login.controller.js
@@ -6,19 +6,40 @@ sap.ui.define([
     "sap/ui/model/json/JSONModel",
     "sap/ui/core/Fragment",
     "sap/m/MessageBox",
+    "sap/m/ColumnListItem",
+    'sap/m/Input'
     
 
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, Token, Filter, FilterOperator, JSONModel, Fragment, MessageBox) {
+    function (Controller, Token, Filter, FilterOperator, JSONModel, Fragment, MessageBox,ColumnListItem,Input) {
         "use strict";
 
         return Controller.extend("com.app.centrallibrary.controller.Login", {
             onInit: function () {
-                debugger;
                 
+                debugger;
+                // this.oTable = this.byId("_IDGenTable1");
+                // this.oReadOnlyTemplate = this.byId("_IDGenTable1").removeItem(0);
+                // // this.rebindTable(this.oReadOnlyTemplate, "Navigation");
+                // this.oEditableTemplate = new ColumnListItem({
+                //     cells: [
+                //         new Input({
+                //             value: "{isbn}"
+                //         }), new Input({
+                //             value: "{title}"
+                        
+                //         }), new Input({
+                //             value: "{price}",
+                            
+                //         }), new Input({
+                //             value: "{author}",
+                            
+                //         })
+                //     ]
+                // });
 
 
                 //  tokens added(Multi-input)
@@ -40,13 +61,13 @@ sap.ui.define([
 
                 
                 const oLocalModel = new JSONModel({
-                    isbn: " ",
-                    title: " ",
-                    quantity: " ",
-                    price: " ",
-                    pages: " ",
-                    author: " ",
-                    status: " "
+                    isbn:"",
+                    title:"",
+                    quantity:"",
+                    price:"",
+                    pages:"",
+                    author:"",
+                    status:""
 
                 });
 
@@ -89,13 +110,14 @@ sap.ui.define([
             setHeaderContext: function () {
                 var oView = this.getView();
                 oView.byId("Bookstitle").setBindingContext(
-                    oView.byId("_IDGenTable1").getBinding("items").getHeaderContext());
+                    oView.byId("_IDGenTable1").getBinding("items").getHeaderContext()).refresh();
+                    this.getView().refresh();
             },
             onCreateBtnPress: async function () {
                 debugger
                 if (!this.oCreateBooksDialog) {
 
-                    this.oCreateBooksDialog = await this.loadFragment("CreateBooks")
+                    this.oCreateBooksDialog = await this.loadFragment("CreateBooks");
 
                 }
 
@@ -127,6 +149,7 @@ sap.ui.define([
                     this.oCreateBooksDialog.close();
                     sap.m.MessageBox.error("Some technical Issue");
                 }
+                location.reload()
             },
 
             //     onSelectBooks:function(){  const { ID } = oEvent.getSource().getSelectedItem().getBindingContext().getObject();
@@ -136,38 +159,74 @@ sap.ui.define([
             //         empName: fName
             //     })
             // },
-           
-            onDeleteBtnPress: async function () {
-                var oSelected = this.byId("_IDGenTable1").getSelectedItem();
-            
-                if (oSelected) {
-                    var oISBN = oSelected.getBindingContext().getObject().isbn;
-            
-                    try {
-                        await oSelected.getBindingContext().delete("$auto");
-                        MessageBox.show(oISBN + " successfully deleted");
-                    } catch (oError) {
-                        MessageBox.show("Deletion Error: " + oError);
-                    }
-            
-                    this.getView().byId("_IDGenTable1").getBinding("items").refresh();
-                } else {
-                    MessageBox.show("Please Select a Row to Delete");
-                }
-            },
-        //     onEditButtonPress: async function(){
-        //         var oSelected = this.byId("_IDGenTable1").getSelectedItem();
-            
-        //         if (oSelected) {
-        //             var oISBN = oSelected.getBindingContext().getObject().isbn;
-        //             try{
-        //                 await oSelected.getBindingContext().loadFragment('BookEdit')
-        //             }catch(oError){
-                        
-        //             }
 
-        //     }
-        // }
+            
+onDeleteBtnPress: async function () {
+    var aSelectedItems = this.byId("_IDGenTable1").getSelectedItems();
+    if (aSelectedItems.length > 0) {
+        var aISBNs = [];
+        aSelectedItems.forEach(function (oSelectedItem) {
+            var sISBN = oSelectedItem.getBindingContext().getObject().isbn;
+            aISBNs.push(sISBN);
+            oSelectedItem.getBindingContext().delete("$auto");
+        });
+
+        Promise.all(aISBNs.map(function (sISBN) {
+            return new Promise(function (resolve, reject) {
+                resolve(sISBN + " Successfully Deleted");
+            });
+        })).then(function (aMessages) {
+            aMessages.forEach(function (sMessage) {
+                MessageToast.show(sMessage);
+            });
+        }).catch(function (oError) {
+            MessageToast.show("Deletion Error: " + oError);
+        });
+
+        this.getView().byId("_IDGenTable1").removeSelections(true);
+        this.getView().byId("_IDGenTable1").getBinding("items").refresh();
+    } else {
+        MessageToast.show("Please Select Rows to Delete");
+    };
+location.reload()
+},
+           
+            // onDeleteBtnPress: async function () {
+            //     var oSelected = this.byId("_IDGenTable1").getSelectedItem();
+            
+            //     if (oSelected) {
+            //         var oISBN = oSelected.getBindingContext().getObject().isbn;
+            
+            //         try {
+            //             await oSelected.getBindingContext().delete("$auto");
+            //             MessageBox.show(oISBN + " successfully deleted");
+            //         } catch (oError) {
+            //             MessageBox.show("Deletion Error: " + oError);
+            //         }
+            
+            //         this.getView().byId("_IDGenTable1").getBinding("items").refresh();
+            //     } else {
+            //         MessageBox.show("Please Select a Row to Delete");
+            //     }
+            // },
+            onEditBtnPress: async function(){
+                // var oSelected = this.byId("_IDGenTable1").getSelectedItem();
+                // const oPayload = this.getView().getModel("localModel").getProperty("/"),
+                //     oModel = this.getView().getModel("ModelV2");
+                // this.aProductCollection = oPayload;
+                // this.rebindTable(this.oEditableTemplate, "Edit");
+            //     var oSelected = this.byId("_IDGenTable1").getSelectedItem();
+            
+            //     if (oSelected) {
+            //         var oISBN = oSelected.getBindingContext().getObject().isbn;
+            //         try{
+            //             await oSelected.getBindingContext().loadFragment('BookEdit')
+            //         }catch(oError){
+                        
+            //         }
+
+            // }
+        }
         });
     });
 
@@ -189,3 +248,23 @@ sap.ui.define([
 // this.oCreateBooksDialog = await this.loadFragment("CreateBooks");
 // });
 // this.getView().addDependent(this.oCreateBooksDialog);
+
+
+// this.oTable = this.byId("idProductsTable");
+// this.oEditableTemplate = new ColumnListItem({
+//     cells: [
+//         new Input({
+//             value: "{Name}"
+//         }), new Input({
+//             value: "{Quantity}",
+//             description: "{UoM}"
+//         }), new Input({
+//             value: "{WeightMeasure}",
+//             description: "{WeightUnit}"
+//         }), new Input({
+//             value: "{Price}",
+//             description: "{CurrencyCode}"
+//         })
+//     ]
+// });
+// },
